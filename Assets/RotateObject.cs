01a34aeab411c587c21e14fc3c1c95ea11eb7786
nitesis <viviane.bendjus@students.fhnw.ch>
2016-03-10 17:35:55 +0100

    added crazy apples flying around
    still not working correct

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+	}
+}
