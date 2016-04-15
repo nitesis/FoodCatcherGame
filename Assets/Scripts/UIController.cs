3c6632aaf13e80b463706b5dd849896d0e2579ee
souzanH <souzanH@users.noreply.github.com>
2016-04-14 21:02:24 -1200

    Script FoodObjectController added. Script Maze generator updated. Maze hard and Maze Medium adapted

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Slider xSlider;
+    public Slider ySlider;
+    public Text xText;
+    public Text yText;
+    public ObjectController objectController;
+    public MazeGenerator mazeGenerator;
+    public GameObject prefab;
+
+    public float x {
+        set {
+            xPosition = (int) value;
+            xText.text = xPosition.ToString();
+        }
+    }
+
+    public float y {
+        set {
+            yPosition = (int) value;
+            yText.text = yPosition.ToString();
+        }
+    }
+
+    private int xPosition;
+    private int yPosition;
+
+    void Start()
+    {
+        xSlider.maxValue = mazeGenerator.width - 1;
+        ySlider.maxValue = mazeGenerator.height - 1;
+        x = xSlider.value;
+        y = ySlider.value;
+    }
+
+    public void spawnObject()
+    {
+        objectController.spawnObject(prefab, new ObjectController.Position(xPosition, yPosition));
+    }
+
+}
