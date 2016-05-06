5b1756114052cb8336d82307e9b1271c2a9dac3f
nitesis <viviane.bendjus@students.fhnw.ch>
2016-05-06 14:48:42 +0200

    clean up

--- Assets/AddPictureToObject.cs
+++ Assets/AddPictureToObject.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class AddPictureToObject : MonoBehaviour {
 
-	//public ArrayList spawnObjects;
 	public Renderer rend;
 	public Texture[] textures;
 	private Shader shad;
@@ -14,23 +13,14 @@ public class AddPictureToObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		var go = GameObject.FindGameObjectWithTag("food");
-//		var rend = go.GetComponent<Renderer>();
 		rend = GetComponent<Renderer>();
 		rend.material.mainTexture = Resources.Load("spiegelei") as Texture;
 		var texture = rend.material.mainTexture;
-//		rend.material.EnableKeyword ("_ALPHATEST_ON");
 		Shader.EnableKeyword("_ALPHATEST_ON");
-//		Shader.DisableKeyword("KEYWORD_OFF");
-
-//		material = foodObject.GetComponent<Renderer>().GetComponent<Material> ();
-//		newTexture = material.mainTexture;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		rend.material.mainTexture = textures[0];
-//		material.mainTexture = newTexture;
+		
 	}
 }
