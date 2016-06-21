4e3a60839f06b0d0075c4b082db0a1ad6b9db226
nitesis <viviane.bendjus@students.fhnw.ch>
2016-06-21 11:41:28 +0200

    quick fixes

--- Assets/Scripts/TestSphereController.cs
+++ Assets/Scripts/TestSphereController.cs
@@ -27,41 +27,7 @@ public class TestSphereController : MonoBehaviour {
 
 		if (other.CompareTag("food"))
         {
-            Texture texture = other.GetComponentInChildren<Renderer>().material.mainTexture;
-            GameObject foodboard = GameObject.Find("Canvas_FoodBoard");
-            Image[] images;
-            images = foodboard.GetComponentInChildren<Image>().GetComponentsInChildren<Image>();
-            // images[2].GetComponentInChildren<ChangeSprite>().changeSprite();
-            for (int i = 0; i < images.Length; i++)
-                if (texture.name + "_sw" == images[i].name)
-                    images[i].GetComponentInChildren<ChangeSprite>().changeSprite();
-
-            /*Transform ImageTr = foodboard.GetComponentInChildren<Image>().transform.Find("apple");
-            Image image = ImageTr.GetComponent<Image>();
-            GetComponentInChildren<Renderer>().material.mainTexture;*/
-
-                    /* Vector3 myPosition = other.transform.position;
-                     Vector3 targetPosition = new Vector3(myPosition.x, 5f, myPosition.z);
-                     Vector3 direction = (targetPosition - myPosition).normalized;
-                     float distance = Vector3.Magnitude(targetPosition - myPosition);
-
-                     Vector3 resultingForceAmount = 50 * direction * distance;
-                     other.GetComponent<Rigidbody>().AddForce(resultingForceAmount);
-
-                     targetPosition = new Vector3(10f, 0f, 8f);
-                     myPosition = other.transform.position;
-                     direction = (targetPosition - myPosition).normalized;
-                     distance = Vector3.Magnitude(targetPosition - myPosition);
-
-                     resultingForceAmount = 10 * direction * distance;
-                     other.GetComponent<Rigidbody>().AddForce(resultingForceAmount);
-
-                     other.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;*/
-
-			//Implement your Faller game object into this variable in the inspector
-			//var FallerObject : GameObject;
-			//monsterfall=Animation
-			//FallerObject.animation.Play("monstersfall");
+            
             Destroy(other.gameObject);
 
         }
