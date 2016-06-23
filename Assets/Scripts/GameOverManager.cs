9183d14e311862a4b913ab69887fab670b8a69b6
nitesis <viviane.bendjus@students.fhnw.ch>
2016-06-23 14:12:01 +0200

    added script

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+	public MazeGenerator mazeGenerator;
+	public float restartDelay = 5f;
+
+	Animator anim;
+	float restartTimer;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (mazeGenerator.foodItemCount <= 0) 
+		{
+			anim.SetTrigger ("GameOver");
+			restartTimer += Time.deltaTime;
+		}
+			if (restartTimer >= restartDelay) 
+		{
+			SceneManager.LoadScene ("menu");
+		}
+	}
+}
