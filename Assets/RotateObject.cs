0616814b30e4989c7e299723f4cea9a007d953ba
souzanH <souzanH@users.noreply.github.com>
2016-06-27 16:22:58 +0300

    templete for Zutaten in easymaze added

--- Assets/RotateObject.cs
+++ Assets/RotateObject.cs
@@ -7,7 +7,7 @@ public class RotateObject : MonoBehaviour {
 
 	void Update ()
 	{
-		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 0, 30) * Time.deltaTime);
 
 	}
 }
