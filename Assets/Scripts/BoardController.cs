9c9bd66f8dadc268ea4d157b79bf134a8ffaa2b8
souzanH <souzanH@users.noreply.github.com>
2016-07-25 15:53:32 +0300

    BoardController script for the generating of board added

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class BoardController : MonoBehaviour {
+
+    public ReciepCSVReader reciepCSVReader;
+	void Start () {
+        LoadSpritesInBoard();       
+    }
+	
+	public void LoadSpritesInBoard()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>();
+        int i = 0;
+        int j = 0;
+        while (i < reciepCSVReader.ReciepList.Count)
+        {
+            buttons[j].GetComponent<Image>().sprite = Resources.Load<Sprite>(reciepCSVReader.ReciepList[i]);
+            buttons[j + 1].GetComponent<Image>().sprite = Resources.Load<Sprite>(reciepCSVReader.ReciepList[i] + "_sw");
+            i++;
+            j += 2;
+        }
+
+    }
+    void Update () {
+	
+	}
+}
