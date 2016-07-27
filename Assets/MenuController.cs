4ab2610091c6f8ab3a2ea82bcc22790e58af7e04
souzanH <souzanH@users.noreply.github.com>
2016-07-27 10:36:20 +0300

    quick fixes

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -70,22 +70,22 @@ public class MenuController : MonoBehaviour {
 	public void MediumMaze_waterGreyStone() {
 		LoadScene ("mediumMaze_waterGreyStone");
 	}
-	public void EasyMaze(string reciepName) {
-        PlayerPrefs.SetString("ChoosedReciep", reciepName);
+	public void EasyMaze() {
+        PlayerPrefs.SetString("ChoosedReciep", "Fondue");
         PlayerPrefs.SetInt("IngredientsCount", 5);
 		LoadScene("easyMaze");
 
     }
 
-    public void MediumMaze(string reciepName, int count) {
-        PlayerPrefs.SetString("ChoosedReciep", reciepName);
-        PlayerPrefs.SetInt("IngredientsCount", count);
+    public void MediumMaze() {
+        PlayerPrefs.SetString("ChoosedReciep", "Aelplermagronen");
+        PlayerPrefs.SetInt("IngredientsCount", 8);
         LoadScene("mediumMaze");
 	}
 
-	public void HardMaze(string reciepName, int count) {
-        PlayerPrefs.SetString("ChoosedReciep", reciepName);
-        PlayerPrefs.SetInt("IngredientsCount", count);
+	public void HardMaze() {
+        PlayerPrefs.SetString("ChoosedReciep", "Capuns");
+        PlayerPrefs.SetInt("IngredientsCount", 9);
         LoadScene("hardMaze");
 	}
 
