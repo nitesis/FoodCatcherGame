bf9b704bd03aaa693bf2556d5d8838dbaa58e7ca
souzanH <souzanH@users.noreply.github.com>
2016-08-15 14:13:54 +0300

    IngredientOut Animation in script added

--- Assets/Scripts/IngredientManager.cs
+++ Assets/Scripts/IngredientManager.cs
@@ -13,7 +13,14 @@ public class IngredientManager : MonoBehaviour {
     float restartTimer;                     // Timer to count up to restarting the level
     private Button pressedbutton;
     private bool pressed;
+    private bool firstRun;
 
+
+    void Start()
+    {
+        pressed = false;
+        firstRun = true;
+    }
     void Awake()
     {
         // Set up the reference.
@@ -23,8 +30,42 @@ public class IngredientManager : MonoBehaviour {
 
     public void buttonPressed(Button btn)
     {
+        if (pressed == false)
+        {
+            GetComponentInChildren<Button>().GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
+            if (firstRun)
+            {
+                anim.SetTrigger("Ingredient");
+                firstRun = false;
+            }
+            else
+                anim.SetTrigger("IngredientIn");
+            pressed = true;
+
+        }
+        else
+        {
+            anim.ResetTrigger("Ingredient");
+            anim.SetTrigger("IngredientOut");
+            pressed = false;
+        }
+    }
+
+    public void ClickToClose()
+    {
+        gameObject.SetActive(false);
+
+    }
+
+    private IEnumerator doProcess(Button btn)
+    {
         GetComponentInChildren<Button>().GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
         anim.SetTrigger("Ingredient");
+        yield return new WaitForSeconds(3f);
+       // gameObject.SetActive(false);
+
+
+
     }
 
     void Update()
